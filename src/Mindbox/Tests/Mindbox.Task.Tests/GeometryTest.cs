87e0ad85b2cb6fa7f0725879adbafccdc0beebbd
Sylifoor <semenbolsakov94@gmail.com>
2023-11-04 20:33:20 +0300

    added tests from triangle class

--- src/Mindbox/Tests/Mindbox.Task.Tests/GeometryTest.cs
+++ src/Mindbox/Tests/Mindbox.Task.Tests/GeometryTest.cs
@@ -16,22 +16,10 @@ namespace Mindbox.Task.Tests
         { 
         }
 
-        [Test]
-        [TestCase(0d, 0d, 0d, false)]
-        public void CreateTriangleTest(double sideA, double sideB, double sideC, bool createSuccess)
-        {
-            Assert.That(Triangle.TryCreate(sideA, sideB, sideC, out var triangle), Is.EqualTo(createSuccess),
-                $"Try create triangle test failed: expected {createSuccess}, but was {!createSuccess}.");
-
-            if(createSuccess)
-            {
-                Assert.That(triangle, Is.Not.Null, "Triangle must be created, but was not.");
-            }
-        }
 
         [Test]
         [TestCase(null, double.NaN)] //temporary
-        public void IFigureCalculateAreaTests(IFigure figure, double expectedArea)
+        public void GetArea_IFigure_Tests(IFigure figure, double expectedArea)
         {
             var actualArea = figure.GetArea();
 
---
+++ src/Mindbox/Tests/Mindbox.Task.Tests/TriangleTests.cs
@@ -0,0 +1,69 @@
+﻿using NUnit.Framework;
+
+namespace Mindbox.Task.Tests
+{
+    public class TriangleTests
+    {
+        [Test]
+        public void GetArea_ValidTriangle_ReturnsCorrectArea()
+        {
+            var triangle = new Triangle(3, 4, 5);
+            var expectedArea = 6;
+
+            var actualArea = triangle.GetArea();
+
+            Assert.That(actualArea, Is.EqualTo(expectedArea), $"Expected area {expectedArea}, but was {actualArea}");
+        }
+
+        [Test]
+        public void GetArea_InvalidTriangle_ThrowsArgumentException()
+        {
+            var triangle = default(Triangle);
+
+            Assert.Throws<ArgumentException>(() => triangle = new Triangle(1, 2, 3), "Triangle is not be created.");
+        }
+
+        [Test]
+        public void GetPerimeter_ValidTriangle_ReturnsCorrectPerimeter()
+        {
+            var triangle = new Triangle(3, 4, 5);
+            var expectedPerimeter = 12;
+
+            var actualPerimeter = triangle.GetPerimeter();
+
+            Assert.That(actualPerimeter, Is.EqualTo(expectedPerimeter), $"Expected perimiter {expectedPerimeter}, but was {actualPerimeter}");
+        }
+
+        [Test]
+        public void TryCreate_ValidTriangle_ReturnsTrueAndTriangle()
+        {
+            var sideA = 3;
+            var sideB = 4;
+            var sideC = 5;
+
+            var result = Triangle.TryCreate(sideA, sideB, sideC, out var triangle);
+
+            Assert.That(result, Is.True, "Triangle must be a created.");
+            Assert.That(triangle, Is.Not.Null, "Triangle is null.");
+            Assert.That(triangle.Value.SideA, Is.EqualTo(sideA),
+                $"Side {sideA} is not equals trianlge side {triangle.Value.SideA}");
+            Assert.That(triangle.Value.SideB, Is.EqualTo(sideB), 
+                $"Side {sideB} is not equals trianlge side {triangle.Value.SideB}");
+            Assert.That(triangle.Value.SideC, Is.EqualTo(sideC), 
+                $"Side {sideC} is not equals trianlge side {triangle.Value.SideC}");
+        }
+
+        [Test]
+        public void TryCreate_InvalidTriangle_ReturnsFalseAndNull()
+        {
+            var sideA = 1;
+            var sideB = 2;
+            var sideC = 3;
+
+            var result = Triangle.TryCreate(sideA, sideB, sideC, out var triangle);
+
+            Assert.That(result, Is.False, "Triangle is not be created.");
+            Assert.That(triangle, Is.Null, "Triangle is not be created.");
+        }
+    }
+}
